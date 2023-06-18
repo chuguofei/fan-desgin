490d934baaa090120a462f28b55f9f85277f1b1e
acfan <acfan0523@gmail.com>
2023-06-18 18:28:13 +0800

    feat: add sass common

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import './tailwind.css'
 import VPDemo from '@/vitepress/components/vp-demo.vue'
 import 'prismjs/themes/prism.css'
 import FanDesign from 'fan-design'
+import '@fan-design/ui-theme'
 
 export default {
   ...Theme,
